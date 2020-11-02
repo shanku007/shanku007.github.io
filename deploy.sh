c39beb3023650eb66b63963625680115f5b21d27
Shankar <jhashankar0077@gmail.com>
2020-11-03 00:00:01 +0530

    Automatic website publish using build command

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
-echo $2
-git add dist && git commit -m $2
+
+# Credit to https://brianchildress.co/named-parameters-in-bash/
+
+dist_folder=${d:-dist}
+message=${m:-Publishing website}
+
+while [ $# -gt 0 ]; do
+
+   if [[ $1 == *"--"* ]]; then
+        param="${1/--/}"
+        declare $param="$2"
+   fi
+
+  shift
+done
+
+echo $dist_folder
+echo $message
+
+git add $dist_folder && git commit -m "$message"
 git subtree push --prefix $1 origin gh-pages
